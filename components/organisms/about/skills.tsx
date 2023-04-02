4cdc5a27bdda085bc71756ab57f2ec118604871c
Mary <101870828+Chioma227@users.noreply.github.com>
2023-04-02 23:14:49 +0100

    feat: created IconLink molecule

--- components/organisms/about/skills.tsx
+++ components/organisms/about/skills.tsx
@@ -1,5 +1,12 @@
+import AnchorLink from "@/components/atoms/link"
+import Img from "@/components/atoms/img"
+
+const linkArr: {}[]=[
+  {}
+]
 
 const Skills = () => {
+
   return (
     <div>
       
